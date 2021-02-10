5f94273e816af9c97a917b1f354f26ea19f8ee02
ctang1174 <Sakura20030>
2021-02-10 16:21:17 -0500

    implemented Discovery character database init and add characters

--- StoreChars.py
+++ StoreChars.py
@@ -1,35 +1,86 @@
 import json
-import os
-from ibm_watson import DiscoveryV2
+from ibm_watson import DiscoveryV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+
 class CharDatabaseInterface:
-    """ store the personality of character text"""
-    def add_char(self, char_personality: dict):
-        """ Adds character personality to database"""
+    """ store the info of character """
+    def add_char(self, char_name: str, char_info: dict):
+        """ Adds character info to database"""
         pass
 
-    def search_char(self, char_personality: dict) -> str:
-        """ Searches for character closest to passed in personality """
+    def search_char(self, char_info: dict) -> str:
+        """ Searches for character closest to passed in info """
         pass
 
 
 class DiscoveryCharDatabase(CharDatabaseInterface):
     api_key = 'hCH8RRcfJEQUE6Ve8gCl_gsXKqM7Mkdusc6Mdnchb51s'
     url = 'https://api.us-south.discovery.watson.cloud.ibm.com/instances/ed199079-a3d5-47be-ae2c-bac80e6582ae'
-    version = '2019-11-22'
+    version = '2019-04-30'
 
-    def __init__(self):
+    def __init__(self, collection_name):
         authenticator = IAMAuthenticator(self.api_key)
-        self.discovery = DiscoveryV2(
+        self.discovery = DiscoveryV1(
             version=self.version,
             authenticator=authenticator
         )
 
         self.discovery.set_service_url(self.url)
 
-    def add_char(self, char_personality: dict):
-        pass
+        environments = self.discovery.list_environments().get_result()
+        environments = [x for x in environments['environments'] if x['name'] != 'Watson System Environment']
+        self.environment_id = environments[0]['environment_id']
+        collections = self.discovery.list_collections(environment_id=self.environment_id).get_result()
+        collections = [x for x in collections['collections'] if x['name'] == collection_name]
+        self.collection_id = collections[0]["collection_id"]
+
+    def create_new_collection(self, collection_name):
+        collection_response = self.discovery.create_collection(
+            environment_id=self.environment_id,
+            name=collection_name).get_result()
+
+        self.collection_id = collection_response["collection_id"]
+
+        return 0
+
+    def add_char(self, char_name: str, char_info: dict):
+        file_name = 'test.json'
+        with open(file_name, 'w') as fp:
+            json.dump(char_info, fp)
+        with open(file_name) as file_info:
+            response = self.discovery.add_document(
+                environment_id=self.environment_id,
+                collection_id=self.collection_id,
+                file=file_info,
+                filename=char_name,
+                file_content_type='application/json'
+            )
+        return response
 
     def search_char(self, char_personality: dict) -> str:
-        pass
+        query_string = ''
+        query_results = self.discovery.query(
+            environment_id=self.environment_id,
+            collection_id=self.collection_id,
+            query=query_string
+        ).get_result()
+        doc_id = query_results["results"][0]["id"]
+
+        response = self.discovery.get_document_status(
+            environment_id=self.environment_id,
+            collection_id=self.collection_id,
+            document_id=doc_id
+        ).get_result()
+
+        return response['filename']
+
+
+if __name__ == "__main__":
+    ddb = DiscoveryCharDatabase("Collection 1")
+    this_dict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+    ddb.search_char(this_dict)
--- TextPersonality.py
+++ TextPersonality.py
@@ -33,7 +33,6 @@ class NLUPersonalityInterface:
         return personality_dict
 
 
-
 if __name__ == "__main__":
     nlu = NLUPersonalityInterface()
     print(nlu.get_personality('But the black kitten had been finished with earlier in the afternoon, and so, while Alice was sitting curled up in a corner of the great arm-chair, half talking to herself and half asleep, the kitten had been having a grand game of romps with the ball of worsted Alice had been trying to wind up, and had been rolling it up and down till it had all come undone again; and there it was, spread over the hearth-rug, all knots and tangles, with the kitten running after its own tail in the middle.'))
