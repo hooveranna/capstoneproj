eb34012dd6529ae100fbbef82318483ca0e2e0b6
lsj1124 <sjl981124@gmail.com>
2021-02-15 19:10:22 -0500

    connect the frontend and backend; comment out unimplemented things

--- mockup.py
+++ mockup.py
@@ -13,18 +13,27 @@ from flask import render_template
 from flask import (
     redirect, request, url_for
 )
+from TextPersonality import NLUPersonalityInterface
+from StoreChars import DiscoveryCharDatabase
 
 app = Flask(__name__)
 run_with_ngrok(app)   #starts ngrok when the app is run
 @app.route("/", methods=('GET','POST'))
 def home(name=None):
     if request.method == 'POST':
-      return redirect(url_for('submit'))  
+        username = request.form['name']
+        usertext = request.form['usertext']
+        return redirect(url_for('submit', username = username, usertext = usertext))
     return render_template('index.html', name=name)
 
-@app.route("/submit")
-def submit(name=None):
-  return render_template('output.html', name=name)
+@app.route("/submit/<username>/<usertext>")
+def submit(username, usertext):
+    nlu = NLUPersonalityInterface()
+    this_dict = nlu.get_personality(usertext)
+    #add condition to handle nlu error
+    ddb = DiscoveryCharDatabase("Collection 1")
+    char_match = ddb.search_char(this_dict)
+    return render_template('output.html', username = username, character_name = char_match)
 
 
 app.run()
