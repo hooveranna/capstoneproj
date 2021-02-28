bbbe01fc03d695bf46eed5c0d7e60298c86874ec
lsj1124 <sjl981124@gmail.com>
2021-02-28 18:53:27 -0500

    add fake test case

---
+++ test/test_personality.py
@@ -0,0 +1,14 @@
+import json
+import urllib
+from TextPersonality import NLUPersonalityInterface
+from ibm_watson import DiscoveryV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def test_persionality():
+    #nlu = NLUPersonalityInterface()
+    # nlu.get_personality(
+    #    'But the black kitten had been finished with earlier in the afternoon, and so, while Alice was sitting curled up in a corner of the great arm-chair, half talking to herself and half asleep, the kitten had been having a grand game of romps with the ball of worsted Alice had been trying to wind up, and had been rolling it up and down till it had all come undone again; and there it was, spread over the hearth-rug, all knots and tangles, with the kitten running after its own tail in the middle.')
+    x = "hello"
+
+    # if condition returns False, AssertionError is raised:
+    assert x == "hello", "x should be 'hello'"
