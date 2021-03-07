e64576a034a759a96ecfe40803c012c346a76552
ctang1174 <Sakura20030>
2021-03-07 00:04:15 -0500

    added functionality to show user the personality of the matched character and changed output of concepts as a unordered list

--- flask_app.py
+++ flask_app.py
@@ -25,23 +25,27 @@ def home(name=None):
 @app.route("/submit/<username>/<usertext>")
 def submit(username, usertext):
     file_name = "test_file.jpeg"
-    file_name2 = "test_file2.jpeg"
+    # file_name2 = "test_file2.jpeg"
+    file_name3 = "character_emotions.jpeg"
+    # file_name4 = "character_concept.jpeg"
     nlu = NLUPersonalityInterface()
     this_dict = nlu.get_personality(usertext)
     create_figure(this_dict[0], file_name)
-    create_figure(this_dict[1], file_name2)
-    if not this_dict[1]:
-        file_name2 = None
-    # add condition to handle nlu error
+    # create_figure(this_dict[1], file_name2)
     ddb = DiscoveryCharDatabase("Collection 1")
     full_dict = dict(this_dict[0])
     full_dict.update(this_dict[1])
-    char_match = ddb.search_char(full_dict)
-    return render_template('output.html', username=username, character_name=char_match, file_name=file_name, file_name2=file_name2)
+    char_match, personality = ddb.search_char(full_dict)
+    emotions, concepts = get_emotions_from_dict(personality)
+    create_figure(emotions, file_name3)
+    # create_figure(personality, file_name4)
+    return render_template('output.html', username=username, character_name=char_match, file_name=file_name, concepts=this_dict[1].keys(), file_name3=file_name3, char_concepts=personality.keys())
+
 
 @app.route("/about-us")
 def about(name=None):
     return render_template('about.html', name=name)
+
 
 @app.errorhandler(InternalServerError)
 def handle_500(e):
@@ -53,5 +57,16 @@ def handle_500(e):
 
     # wrapped unhandled error
     return render_template("500_unhandled.html", e=original), 500
+
+
+def get_emotions_from_dict(personality):
+    emotions_dict = dict()
+    emotions_dict["disgust"] = personality.pop("disgust", 0)
+    emotions_dict["joy"] = personality.pop("joy", 0)
+    emotions_dict["anger"] = personality.pop("anger", 0)
+    emotions_dict["sadness"] = personality.pop("sadness", 0)
+    emotions_dict["fear"] = personality.pop("fear", 0)
+    return emotions_dict, personality
+
 
 app.run()
